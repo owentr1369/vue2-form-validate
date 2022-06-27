e66c5b457b20a4b29a946563d0cfb1bb632264b0
TranTam <tamvaa1306@gmail.com>
2022-06-27 15:10:12 +0700

    styling form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -9,4 +9,30 @@ export default {};
 </script>
 
 <style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input {
+  display: block;
+  padding: 10px 6px;
+  widows: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
 </style>
